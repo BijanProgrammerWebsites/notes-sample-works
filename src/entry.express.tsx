d17aa0a71995981b8ba5238014c5e4482e14312f
BijanProgrammer <bijaneisapour@gmail.com>
2023-04-21 14:41:31 +0330

    [CI/CD][FIX] base url

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -40,7 +40,7 @@ const app = express();
 
 // Static asset handlers
 // https://expressjs.com/en/starter/static-files.html
-app.use(`/build`, express.static(buildDir, { immutable: true, maxAge: '1y' }));
+app.use(`/sample-works/build`, express.static(buildDir, { immutable: true, maxAge: '1y' }));
 app.use(express.static(distDir, { redirect: false }));
 
 // Use Qwik City's page and endpoint request handler
